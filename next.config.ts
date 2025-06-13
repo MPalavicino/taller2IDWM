d01d5971cd3fae4c079c8a14b40655663cd288d0
pala130820 <106504008+MPalavicino@users.noreply.github.com>
2025-06-12 21:50:54 -0400

    Feat: Add configuration for image optimization, update dependencies, and enhance global styles

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  Images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ]  
+    }  
 };
 
 export default nextConfig;
