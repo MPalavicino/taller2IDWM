fb94c83ddf14c5abc86e802aee6f549ce264c399
Jmanu866 <130512420+Jmanu866@users.noreply.github.com>
2025-06-14 01:30:20 -0400

    Feat: Implement ViewProductPage and integrate product fetching logic

---
+++ src/views/products/ViewProductPage.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { Product } from "@/interfaces/Product";
+import { useProductStore } from "@/stores/ProductStore"
+import { use, useEffect, useState } from "react";
+
+
+
+export default function ViewProductPage() {
+
+    const { products, loading, fetchProducts, filters } = useProductStore();
+    const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+    useEffect(() => {
+        fetchProducts();
+    }, [filters]);
+
+    if (loading) {
+        return (
+            <div className="flex items-center justify-center min-h-screen bg-gray-100">
+                <p className="text-lg text-gray-700">Cargando Productos</p>
+            </div>
+        );
+    }
+
+    return (
+        <div className="min-h-screen bg-gray-100">
+            {/*Navbar */}
+
+            <nav className="bg-blue-700 shadow mb-4">
+                <div className="max-w-7xl mx-auto px-4 py-2 flex items-center justify-between">
+                    {/* Nombre de la tienda */}
+                    <h1 className="text-xl font-bold text-white">IDWM</h1>
+
+                    {/* Botón Iniciar Sesión */}
+                    <a
+                        href="/login"
+                        className="text-sm bg-white text-blue-700 font-semibold px-4 py-2 rounded hover:bg-blue-100 transition"
+                    >
+                        Iniciar sesión
+                    </a>
+                </div>
+            </nav>
+
+            {/*Banner */}
+            <div className="relative h-64 bg-gray-200 ">
+
+                <div className="absolute inset-0 bg-cover bg-center " style={{ backgroundImage: `url('/tienda.jpg')` }} />
+                <div className="absolute inset-0 bg-black opacity-50" />
+                <div className="relative flex items-center justify-center h-full">
+                    <h2 className="text-3xl text-white font-bold">Bienvenido a nuestra tienda</h2>
+                </div>
+
+
+
+
+            </div>
+        </div>
+
+    )
+}
