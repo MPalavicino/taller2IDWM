e380b7a3d483ea72cc27c0b392d174fe2ed72ef4
pala130820 <106504008+MPalavicino@users.noreply.github.com>
2025-07-06 01:38:10 -0400

    Feat: Enhance ProductCard component with cart functionality and user authentication dialog

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -1,31 +1,47 @@
-import { Product } from "@/interfaces/Product";
-import Image from "next/image";
-
+import { Product } from '../../interfaces/Product';
+import Image from 'next/image';
+import { Button } from '../ui/button';
+import { useAuth } from '@/hooks/useAuth';
+import { LoginDialog } from './LoginDialog';
+import { useState } from 'react';
+import { useCartStore } from '../../stores/CartStore';
 
 interface ProductCardProps {
     product: Product;
-    onClick: () => void;
+    onClick?: () => void;
 }
+
 export const ProductCard = ({ product, onClick }: ProductCardProps) => {
+    const { addToCart } = useCartStore()
+
+    const { user } = useAuth();
+    const [showDialog, setShowDialog] = useState(false);
+    const handleAddToCart = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        if (!user) {
+            setShowDialog(true);
+            return;
+        }
+        addToCart(product.id, 1);
+        alert(`Producto ${product.name} agregado al carrito.`);
+    }
+
     return (
-        <div className="bg-white shadow-md rounded-lg overflow-hidden cursor-pointer hover:scale-105 transition"
-            onClick={onClick}>
-            <div className="relative w-full h-48 bg-gray-200 ">
-                <Image src="/Producto.jpg" alt={product.name} fill className='object-cover' />
+        <>
+            <div className='bg-white shadow-md rounded-lg overflow-hidden cursor-pointer hover:scale-105 transition'
+                onClick={onClick}>
+                <div className='relative w-full h-48 bg-gray-200 flex items-center justify-center'>
+                    <Image src="/Producto.jpg" alt={product.name} width={200} height={200} className='object-contain' />
+                </div>
+                <div className='p-4'>
+                    <h3 className='font-semibold text-lg'>{product.name}</h3>
+                    <p className='mt-2 text-blue-700 font-bold text-xl'>${product.price}</p>
+                    <Button className='mt-4 w-full' onClick={handleAddToCart}>Agregar al Carrito</Button>
+                </div>
             </div>
 
-            <div className="p-4">
-                <h3 className="text-lg font-semibold text-gray-800">{product.name}</h3>
-                <p className="text-gray-600 mt-2">{product.description}</p>
-                <p className="text-blue-600 font-bold mt-2">{product.price.toLocaleString('es-CL', {
-                    style: 'currency',
-                    currency: 'CLP',
-                    minimumFractionDigits: 0,
-                    maximumFractionDigits: 0
-                })}</p>
-            </div>
-
-        </div>
+            <LoginDialog open={showDialog} onClose={()=>setShowDialog(false)}/>
+        </>
 
     )
 }
